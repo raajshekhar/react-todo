07fde11f9e929506b73112dd32508f9a770a10ec
Rajashekhar <rajashekhar.vankayala@sooryen.com>
2019-04-21 15:27:01 +0530

    Updates

---
+++ precache-manifest.fcac1a2015e6dad9fa16c1c60cedd103.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  },
+  {
+    "revision": "18c7e93ee1d62164f59a",
+    "url": "/react-todo/static/js/main.70bc9aea.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-todo/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "18c7e93ee1d62164f59a",
+    "url": "/react-todo/static/css/main.5fb55b5f.chunk.css"
+  },
+  {
+    "revision": "7e6504dfa9e158908732d0f43f98322d",
+    "url": "/react-todo/index.html"
+  }
+];
